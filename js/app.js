f672325bd25a9c1b9cc59d76ef388bcb03fc2c2d
Afolabi Oluwatobiloba Ajibola Richard <169098104+Trippsy@users.noreply.github.com>
2025-02-20 18:16:26 +0100

    fix:submit form logic

--- js/app.js
+++ js/app.js
@@ -12,18 +12,18 @@ menu.addEventListener('click', function () {
     console.log(Display);
   }
 })
-const button = document.querySelector('.button');
-const inputs = document.querySelectorAll('input');
-const form = document.querySelector('form');
-form.addEventListener('submit',function(event) {
+// const button = document.querySelector('.button');
+// const inputs = document.querySelectorAll('input');
+// const form = document.querySelector('form');
+// form.addEventListener('submit',function(event) {
 
-  event.preventDefault();
-  const name = event.currentTarget.name.value;
-  console.log(name);
-  const email = event.currentTarget.email.value;
-  console.log(email);
-  const project = event.currentTarget.project.value;
-  console.log(project);
-  const message = event.currentTarget.message.value;
-  console.log(message);
-});
+//   event.preventDefault();
+//   const name = event.currentTarget.name.value;
+//   console.log(name);
+//   const email = event.currentTarget.email.value;
+//   console.log(email);
+//   const project = event.currentTarget.project.value;
+//   console.log(project);
+//   const message = event.currentTarget.message.value;
+//   console.log(message);
+// });
